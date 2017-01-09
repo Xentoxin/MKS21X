4c65953ee1197e6e5011f3725c9a38e703dd6b0b
hao chen <hao chen>
2017-01-09 12:15:49 -0800

    Revisions

--- Traveling Salesman Problem/City.java
+++ Traveling Salesman Problem/City.java
@@ -1,34 +1,42 @@
+import java.util.Random;
 
 public class City {
-	private String name;
-	private double XPOS, YPOS;
-
-	public City(String n, double x, double y) {
-		this.name = n;
-		this.XPOS = x;
-		this.YPOS = y;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public double getX() {
-		return this.XPOS;
-	}
-
-	public double getY() {
-		return this.YPOS;
-	}
-
-	public double distance(City c) {
-		double deltaX = c.XPOS - this.XPOS;
-		double deltaY = c.YPOS - this.YPOS;
-		double distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-		return distance;
-	}
-
-	public String toString() {
-		return getName();
-	}
+private double XPOS, YPOS;
+public boolean visited = false;
+public City(){
+	Random rand = new Random();
+	this.XPOS = rand.nextInt(500);
+	this.YPOS = rand.nextInt(500);
 }
+public double getX(){
+	return this.XPOS;
+}
+public void setX(double n){
+	this.XPOS = n;
+}
+public double getY(){
+	return this.YPOS;
+}
+public void setY(double n){
+	this.YPOS = n;
+}
+public boolean isVisited(){
+	return visited;
+}
+public void setVisited(boolean n){
+	this.visited = n;
+}
+
+public double Distance(City other){
+	double deltaX = other.XPOS - this.XPOS;
+	double deltaY = other.YPOS - this.YPOS;
+	double distance  = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+	return distance;
+}
+
+}
+
+//so i have an xy position for every city
+//i can find the distance between each city
+//have a list of cities
+//randomly pick 
--- Traveling Salesman Problem/Route.java
+++ Traveling Salesman Problem/Route.java
@@ -1,29 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Route {
-	public Route(ArrayList<City> Given) {
-		setCities(Given);
-	}
+public int amount = 3;
+private City route[] = new City[amount];
 
-	public Route(Route other) {
-		// constructor for route given an older route
-		// basically clones it
-		other.cities.stream().forEach(x -> cities.add(x));
+public Route(){
+	for(int i = 0 ;  i < getRoute().length ; i ++){
+		getRoute()[i] = new City();
 	}
+}
+public City[] getRoute(){
+	return route;
+}
+public void setRoute(City r[]){
+	this.route = r;
+}
 
-	private ArrayList<City> cities = new ArrayList<City>();
-
-	public ArrayList<City> getCities() {
-		return cities;
-	}
-
-	public void setCities(ArrayList<City> A) {
-		this.cities = A;
-	}
-
-	public String toString() {
-		// converts cities arraylist to array then makes that into a string
-		return Arrays.toString(cities.toArray());
-	}
 }
